be1a1c3c5d2a71493279be96c260af36d1b4b33e
limyeeun <ifg.035@daum.net>
2023-10-30 16:06:47 +0900

    mobile navi 완료

---
+++ script/main.js
@@ -0,0 +1,19 @@
+//moblie navi
+$(function(){
+let ismoblie= screen.availWidth > 320 && screen.availWidth <=600;
+let isClicked = true
+    $(".mo-menu").on("click",function(){
+        if(ismoblie){
+            console.log("현재 :"+isClicked);
+            if(isClicked){
+                $(".mo-menu span").text("close");
+                $(".navi").css("display","block");
+                isClicked = false;
+            } else{
+                $(".mo-menu span").text("menu");
+                $(".navi").css("display","none");
+                isClicked = true;
+            }
+        }
+    })
+});
